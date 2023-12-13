9117c5a91090def9bcbdd5f13ad1c790be730990
sekar gesti a. u <95660646+sekargestiau@users.noreply.github.com>
2023-12-13 19:14:47 +0700

    update get order data based on collector & user ID

--- Cloud Computing/controllers/order-controller.js
+++ Cloud Computing/controllers/order-controller.js
@@ -310,6 +310,92 @@ module.exports = {
         });
     },
 
+    getAllOrderDataUser: (req, res) => {
+        const userId = req.params.id;
+
+        if(!userId){
+            return res.status(400).json({
+                error: 'Bad request: Missing user ID',
+            });
+        }
+
+        const getUserOrder = `SELECT * FROM orders WHERE user_id = ? AND order_status != 'delivered'`;
+            
+        db.query(getUserOrder, [userId], (error, results) => {
+            if (error) {
+                console.error('Error retrieving user order data:', error);
+                return res.status(500).json({
+                    error: 'An internal server error occured',
+                });
+            }
+            if (results.length === 0) {
+                return res.status(404).json({
+                    message: 'No order data not found',
+                })
+            }
+            const userOrderData = results.map(order => ({
+                order_id: order.id,
+                user_id: order.user_id,
+                collector_id: order.collector_id,
+                waste_type: order.waste_type,
+                waste_qty: order.waste_qty,
+                user_notes: order.user_notes,
+                recycle_fee: order.recycle_fee,
+                pickup_fee: order.pickup_fee,
+                subtotal_fee: order.subtotal_fee,
+                order_status: order.order_status,
+                order_datetime: order.order_datetime,
+                pickup_datetime: order.pickup_datetime,
+                pickup_latitude: order.pickup_latitude,
+                pickup_longitude: order.pickup_longitude,
+            }));
+            res.status(200).json(userOrderData);
+        });
+    },
+
+    getAllOrderDataCollector: (req, res) => {
+        const collectorId = req.params.id;
+
+        if(!collectorId){
+            return res.status(400).json({
+                error: 'Bad request: Missing user ID',
+            });
+        }
+
+        const getCollectorOrder = `SELECT * FROM orders WHERE collector_id = ? AND order_status != 'delivered'`;
+            
+        db.query(getCollectorOrder, [collectorId], (error, results) => {
+            if (error) {
+                console.error('Error retrieving user order data:', error);
+                return res.status(500).json({
+                    error: 'An internal server error occured',
+                });
+            }
+            if (results.length === 0) {
+                return res.status(404).json({
+                    message: 'No order data not found',
+                })
+            }
+            const collectorOrderData = results.map(order => ({
+                order_id: order.id,
+                user_id: order.user_id,
+                collector_id: order.collector_id,
+                waste_type: order.waste_type,
+                waste_qty: order.waste_qty,
+                user_notes: order.user_notes,
+                recycle_fee: order.recycle_fee,
+                pickup_fee: order.pickup_fee,
+                subtotal_fee: order.subtotal_fee,
+                order_status: order.order_status,
+                order_datetime: order.order_datetime,
+                pickup_datetime: order.pickup_datetime,
+                pickup_latitude: order.pickup_latitude,
+                pickup_longitude: order.pickup_longitude,
+            }));
+            res.status(200).json(collectorOrderData);
+        });
+    },
+
     getOrderHistoryCollector: (req, res) => {
         const collectorId = req.params.id;
 
