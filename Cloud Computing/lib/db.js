d0753b6fb6570f1d46f4ad9cffe4427300d477e3
Nabilla Nurulita Dewi <87643077+nabilland@users.noreply.github.com>
2023-12-15 14:51:06 +0700

    Update db.js

--- Cloud Computing/lib/db.js
+++ Cloud Computing/lib/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
+    connectionLimit: 10,
     host: "34.101.86.165",
     user: "root",
     database: "api_backend",
-    password: "password",
+    password: "password"
 })
 
-connection.connect();
 module.exports = connection;
